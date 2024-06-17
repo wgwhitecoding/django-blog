5b8c64fbb30558c72c3c0011247911bcc83e0d65
willcodinggod white <walidwhite@live.co.uk>
2024-06-17 12:47:19 +0000

    installed database again

--- codestar/settings.py
+++ codestar/settings.py
@@ -86,24 +86,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #   }
 #}
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-# Debug print to check the DATABASE_URL value
-print(f"DATABASE_URL: {DATABASE_URL}")
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
